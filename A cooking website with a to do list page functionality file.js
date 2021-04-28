ec319035c80f479f53fed1b3cca177385ed9c3b5
RuaaAlsugheir <alsugheirruaa@gmail.com>
2021-04-28 23:02:44 +0300

    started make the skeleton for the recipes section

--- A cooking website with a to do list page functionality file.js
+++ A cooking website with a to do list page functionality file.js
@@ -1,5 +1,18 @@
 const body = $(`body`);
+$(`#recipeDisplay`).hide();
 const startButton = $(`#startPoint`);
 startButton.on(`click`, () => {
-    
-}) 
+  $(`#homePage`).hide();
+  $(`#recipeDisplay`).show();
+  $(`#breakfast`).hide();
+  $(`#lunchMainDishes`).hide();
+  $(`#desserts`).hide();
+  $(`#dinner`).hide();
+});
+
+const entranceButtons = $(`.recipeEntrance`);
+entranceButtons.forEach(element => {
+    element.on(`click`, (event) => {
+        
+    })
+});
